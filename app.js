ac8e8d74767352e31e796d6af5aff24772e1889b
himanishpuri <himanishpuri2203@gmail.com>
2024-08-18 09:28:17 +0530

    added db connection

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+	cors({
+		origin: process.env.HOST_ORIGIN,
+		credentials: true,
+	}),
+);
+app.use(cookieParser());
+
+export default app;
